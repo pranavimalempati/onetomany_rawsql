4edf15fd7c63738a14d46789e844cb34cd411f95
pranavimalempati <pranavi.malempati@tectoro.com>
2022-12-26 19:15:51 +0530

    initailcommit

--- router/router.js
+++ router/router.js
@@ -5,9 +5,7 @@ const router = require("express").Router();
 router.post("/insert", Service.add);
 router.post("/find", Service.find);
 router.put("/update", Service.update);
-// router.put("/updatedept", Service.updatedept);
 router.delete("/delete", Service.remove);
-// router.delete("/deleteemp/:emp_id", Service.deleteemp);
 
 
 
